0c1b655ed2f420e3a77f790c5550524ee039ec75
mac <1961473915@qq.com>
2017-09-14 14:27:50 +0800

    完整二维码

--- app/src/main/java/com/jd/jarvisdemo1/ui/ZxingAct.java
+++ app/src/main/java/com/jd/jarvisdemo1/ui/ZxingAct.java
@@ -1,12 +1,23 @@
 package com.jd.jarvisdemo1.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.jd.jarvisdemo1.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by JarvisDong on 2017/9/13.
@@ -21,8 +32,45 @@ public class ZxingAct extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ZxingAct.this,CaptureActivity.class));
+                enterQRcode();
             }
         });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = generateBitmap("感谢你为人民作出的贡献!", 300, 300);
+                if (bitmap != null) {
+                    ((ImageView) findViewById(R.id.img)).setImageBitmap(bitmap);
+                }
+            }
+        });
+    }
+
+    private void enterQRcode() {
+        startActivity(new Intent(this, CaptureActivity.class));
+    }
+
+    private Bitmap generateBitmap(String content, int width, int height) {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, String> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        try {
+            BitMatrix encode = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+            int[] pixels = new int[width * height];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (encode.get(j, i)) {
+                        pixels[i * width + j] = 0x00000000;
+                    } else {
+                        pixels[i * width + j] = 0xffffffff;
+                    }
+                }
+            }
+            return Bitmap.createBitmap(pixels, 0, width, width, height, Bitmap.Config.RGB_565);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
